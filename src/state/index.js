699193a52c29e79bced6c1af28add2f726df0e56
Jason Bahl <jasonbahl@mac.com>
2019-06-11 09:37:06 -0700

    - Add Apollo Logger on the client

--- src/state/index.js
+++ src/state/index.js
@@ -1,6 +1,7 @@
 import { InMemoryCache } from "apollo-cache-inmemory"
-import { HttpLink } from "apollo-link-http"
+import { BatchHttpLink } from "apollo-link-batch-http";
 import { ApolloClient } from "apollo-client"
+import apolloLogger from 'apollo-link-logger';
 import { split, ApolloLink } from "apollo-link"
 import { WebSocketLink } from "apollo-link-ws"
 import { getMainDefinition } from "apollo-utilities"
@@ -10,25 +11,28 @@ const GRAPHQL_PORT = process.env.REACT_APP_GRAPHQL_PORT || 3010
 
 export const cache = new InMemoryCache()
 
-const httpLink = new HttpLink({
-  uri: `http://localhost:${GRAPHQL_PORT}/graphql`,
-})
+const batchHttpLink = new BatchHttpLink({
+    uri: `http://localhost:${GRAPHQL_PORT}/graphql`,
+    headers: {
+        batch: "true"
+    }
+});
 
 const webSocketLink = new WebSocketLink({
-  uri: `ws://localhost:${GRAPHQL_PORT}/graphql`,
+    uri: `ws://localhost:${GRAPHQL_PORT}/graphql`,
 })
 
 const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === "OperationDefinition" && operation === "subscription"
-  },
-  webSocketLink,
-  ApolloLink.from([clientStateLink, httpLink])
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === "OperationDefinition" && operation === "subscription"
+    },
+    webSocketLink,
+    ApolloLink.from([apolloLogger, clientStateLink, batchHttpLink]),
 )
 
 export const client = new ApolloClient({
-  link,
-  cache,
-  connectToDevTools: true,
+    link,
+    cache,
+    connectToDevTools: true,
 })
