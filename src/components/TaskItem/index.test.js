8ab3ce812ef3b76977a6bf17ecff18d2505f111e
Jason Bahl <jasonbahl@mac.com>
2019-06-12 14:16:00 -0700

    - Add test for component

--- src/components/TaskItem/index.test.js
+++ src/components/TaskItem/index.test.js
@@ -3,49 +3,49 @@ import { MockedProvider } from "react-apollo/test-utils"
 import TaskItem from "./index.js"
 import TestRenderer from "react-test-renderer"
 import wait from "waait"
-import {TASKS_QUERY} from "../TaskList";
+import { TASKS_QUERY } from "../TaskList"
 
 const mocks = [
-    {
-        request: {
-            query: TASKS_QUERY,
-            variables: {
-                filters: null,
-            },
-        },
-        result: {
-            data: {
-                tasks: [
-                    {
-                        id: 1,
-                        title: "New Task",
-                        taskStatus: "COMPLETE",
-                        category: {
-                            id: 1,
-                            name: "Home",
-                            color: "red",
-                        },
-                    },
-                    {
-                        id: 2,
-                        title: "Another New Task",
-                        taskStatus: "COMPLETE",
-                        category: {
-                            id: 1,
-                            name: "Home",
-                            color: "red",
-                        },
-                    },
-                ],
-            },
-        },
+  {
+    request: {
+      query: TASKS_QUERY,
+      variables: {
+        filters: null,
+      },
     },
+    result: {
+      data: {
+        tasks: [
+          {
+            id: 1,
+            title: "New Task",
+            taskStatus: "COMPLETE",
+            category: {
+              id: 1,
+              name: "Home",
+              color: "red",
+            },
+          },
+          {
+            id: 2,
+            title: "Another New Task",
+            taskStatus: "COMPLETE",
+            category: {
+              id: 1,
+              name: "Home",
+              color: "red",
+            },
+          },
+        ],
+      },
+    },
+  },
 ]
 
-it('renders without crashing', () => {
-    TestRenderer.create(
-        <MockedProvider mocks={mocks} addTypename={false}>
-            <TaskItem/>
-        </MockedProvider>
-    );
-});
+it("renders without crashing", () => {
+  TestRenderer.create(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <TaskItem />
+    </MockedProvider>
+  )
+})
